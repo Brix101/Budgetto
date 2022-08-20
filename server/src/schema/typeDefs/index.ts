d6e72eb8af20a51e939483e9fc5619ffa29abb46
Brixter Porras <brixterporras@gmail.com>
2022-08-20 22:08:49 +0800

    Refactor naming for type & resolver functions

--- server/src/schema/typeDefs/index.ts
+++ server/src/schema/typeDefs/index.ts
@@ -1,4 +1,5 @@
 import expensesType from "./expenses.type";
+import frequencyType from "./frequency.type";
 import incomeType from "./income.type";
 
-export default [expensesType, incomeType];
+export default [expensesType, frequencyType, incomeType];
