f0bf733de73394251285737b37979e7baece003f
Brixter Porras <brixterporras@gmail.com>
2022-08-21 23:35:42 +0800

    Added createExpense mutation function

--- server/src/resolvers/expenses.resolver.ts
+++ server/src/resolvers/expenses.resolver.ts
@@ -1,9 +1,25 @@
-import { Query } from "type-graphql";
+import { Arg, Mutation, Query } from "type-graphql";
 import { Expenses } from "../entity";
+import { CreateExpenseInput } from "../entity/expenses.entity";
 
 export class ExpensesResolver {
   @Query(() => [Expenses])
   getAllExpenses() {
     return Expenses.find();
+  }
+
+  @Mutation(() => Expenses)
+  async createExpenses(
+    @Arg("input") input: CreateExpenseInput
+    // @Ctx() context: Context
+  ) {
+    const expenses = new Expenses();
+    expenses.name = input.name;
+    expenses.amount = input.amount;
+    expenses.category = input.category;
+    expenses.note = input.note;
+    // expenses.frequency = input.frequency;
+
+    return await expenses.save();
   }
 }
